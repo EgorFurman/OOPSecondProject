f917fccfb89a3f75568f2c342da99ce49d0773b7
egord <egordedkov01@gmail.com>
2024-05-05 20:50:47 +0500

    Много
    1) Удалил дандер метод bool из классов Cell и TicTacToe ввиду неочевидности возвращаемого значения.
    
    2) Добавил методы is_empty и is_field для класса Cell.
    
    3) Добавил метод play в класс TicTacToe.
    
    4) Переименовал некоторые переменные и методы, упростил проверки в методе computer_go, постарался сделать код более читаемым.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from random import randint
-from typing import Any
+from random import randint, shuffle
 from itertools import chain
 
 
@@ -9,17 +8,14 @@ class Cell:
     def __init__(self):
         self.value = 0
 
+    def __str__(self):
+        return self.__STRING_REPRESENT[self.value]
+
     def is_empty(self):
         return self.value == 0
 
     def is_field(self):
         return self.value != 0
-
-    def __bool__(self) -> bool:
-        return not self.value
-
-    def __str__(self):
-        return self.__STRING_REPRESENT[self.value]
 
 
 class BoolDescriptor:
@@ -29,7 +25,9 @@ class BoolDescriptor:
     def __get__(self, instance, owner):
         return getattr(instance, self.name)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance, value: bool):
+        if not isinstance(value, bool):
+            raise ValueError('Значение')
         setattr(instance, self.name, value)
 
 
@@ -44,6 +42,8 @@ class TicTacToe:
 
     def __init__(self):
         self.__pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
+
+        self.__lines = self.__get_lines()
 
         self.is_human_win = False
         self.is_computer_win = False
@@ -61,22 +61,33 @@ class TicTacToe:
 
         self.__pole[row][col].value = value
 
-        columns = self.__get_columns()
+    def play(self):
+        step_game = 0
 
-        self.is_human_win = any(all(cell.value == self.HUMAN_X for cell in line) for line in columns)
-        self.is_computer_win = any(all(cell.value == self.COMPUTER_O for cell in line) for line in columns)
-        self.is_draw = not self.is_human_win and not self.is_computer_win and not any(cell for cell in chain(*self.__pole))
+        while not (self.is_draw or self.is_human_win or self.is_computer_win):
+            self.show()
 
-    def __bool__(self):
-        return not (self.is_human_win or self.is_computer_win or self.is_draw)
+            if step_game % 2 == 0:
+                game.human_go()
+            else:
+                game.computer_go()
+            step_game += 1
 
-    def init(self) -> None:
-        for cell in chain(*self.__pole):
-            cell.value = self.FREE_CELL
+            if self.__is_win():
+                self.is_human_win = self.__is_human_win()
+                self.is_computer_win = self.__is_computer_win()
+            self.is_draw = self.__is_draw()
 
-        self.is_human_win = False
-        self.is_computer_win = False
-        self.is_draw = False
+        self.show()
+
+        if game.is_human_win:
+            print("The future is not set. There is no fate but what we make for ourselves.")
+        elif game.is_computer_win:
+            print("You are terminated.")
+        else:
+            print("Cyborgs don't feel pain. You do.")
+
+        self.__init__()
 
     def show(self) -> None:
         # Выводим верхнюю горизонтальную линию
@@ -99,50 +110,8 @@ class TicTacToe:
         # Выводим нижнюю горизонтальную линию
         print("  " + "└───" * (len(self.__pole)) + "┘")
 
-    def human_go(self):
+    def human_go(self) -> None:
         self[self.__get_user_step()] = self.HUMAN_X
-
-    def computer_go(self) -> None:
-        columns = self.__get_columns()
-
-        is_not_cell = lambda cell: not cell
-
-        for column in columns:
-            filled_cells = list(filter(is_not_cell, column))
-
-            if len(filled_cells) == 2 and all(map(lambda cell: cell.value==self.COMPUTER_O, filled_cells)):
-                for cell in column:
-                    if cell:
-                        cell.value = self.COMPUTER_O
-                        self.is_computer_win = any(
-                            all(cell.value == self.COMPUTER_O for cell in line) for line in columns)
-                        return
-
-        for column in columns:
-            filled_cells = list(filter(is_not_cell, column))
-
-            if len(filled_cells) == 2 and all(map(lambda cell: cell.value == self.HUMAN_X, filled_cells)):
-                for cell in column:
-                    if cell:
-                        cell.value = self.COMPUTER_O
-                        self.is_computer_win = any(
-                            all(cell.value == self.COMPUTER_O for cell in line) for line in columns)
-                        return
-
-        if self.__pole[1][1]:
-            self[1, 1] = self.COMPUTER_O
-            return
-
-        for row, col in ((0, 0), (0, 2), (2, 0), (2, 2)):
-            if self.__is_free_cell(row, col):
-                self[row, col] = self.COMPUTER_O
-                return
-
-        while True:
-            row, col = randint(0, 2), randint(0, 2)
-            if self.__is_free_cell(row, col):
-                self[row, col] = self.COMPUTER_O
-                return
 
     def __get_user_step(self) -> tuple[int, int]:
         while True:
@@ -152,30 +121,76 @@ class TicTacToe:
             try:
                 step = tuple(map(lambda x: int(x.strip()) - 1, res.split(',')))
                 self.__verify_indices(*step)
-
-                if not self.__is_free_cell(*step):
+                if not self[*step] == self.FREE_CELL:
                     raise IndexError('Указанная клетка уже занята.')
             except IndexError as ex:
                 print(ex)
                 continue
             except Exception:
-                print('неверный формат введенных данных.')
+                print('Неверный формат введенных данных.')
                 continue
-
             return step
 
-    def __verify_indices(self, row: Any, col: Any) -> None:
+    def computer_go(self) -> None:
+        angle_cells = [(0, 0), (0, 2), (2, 0), (2, 2)]
+        shuffle(angle_cells)
+
+        for column in self.__lines:
+            filled_cells = list(filter(lambda cell: cell.is_field(), column))
+
+            if len(filled_cells) == 2 and all(map(lambda cell: cell.value == self.COMPUTER_O, filled_cells)):
+                for cell in column:
+                    if cell.is_empty():
+                        cell.value = self.COMPUTER_O
+                        return
+
+        for column in self.__lines:
+            filled_cells = list(filter(lambda cell: cell.is_field(), column))
+
+            if len(filled_cells) == 2 and all(map(lambda cell: cell.value == self.HUMAN_X, filled_cells)):
+                for cell in column:
+                    if cell.is_empty():
+                        cell.value = self.COMPUTER_O
+                        return
+
+        if self[1, 1] == self.FREE_CELL:
+            self[1, 1] = self.COMPUTER_O
+            return
+
+        for row, col in angle_cells:
+            if self[row, col] == self.FREE_CELL:
+                self[row, col] = self.COMPUTER_O
+                return
+
+        while True:
+            row, col = randint(0, 2), randint(0, 2)
+            if self[row, col] == self.FREE_CELL:
+                self[row, col] = self.COMPUTER_O
+                return
+
+    def __is_computer_win(self) -> bool:
+        return not self.__is_human_win()
+
+    def __is_human_win(self) -> bool:
+        for line in self.__lines:
+            if all(cell.is_field() for cell in line):
+                return all(cell.value == self.HUMAN_X for cell in line)
+
+    def __is_win(self):
+        return any(all(c.value != self.FREE_CELL and c.value == line[0].value for c in line) for line in self.__lines)
+
+    def __is_draw(self):
+        return all(cell.is_field() for cell in chain(*self.__pole))
+
+    def __verify_indices(self, row: int, col: int) -> None:
         if not self.__is_game_cell_coordinates(row, col):
             raise IndexError('Клетка с указанными индексами не существует.')
 
-    def __get_columns(self) -> tuple:
+    def __get_lines(self) -> tuple:
         row1, row2, row3 = (row for row in self.__pole)
         col1, col2, col3 = (tuple(row[i] for row in self.__pole) for i in range(3))
         main_dig, side_dig = tuple(self.__pole[i][i] for i in range(3)), tuple(self.__pole[i][-1 - i] for i in range(3))
         return row1, row2, row3, col1, col2, col3, main_dig, side_dig
-
-    def __is_free_cell(self, row: int, col: int) -> bool:
-        return self.__pole[row][col].is_empty()
 
     @staticmethod
     def __is_game_cell_coordinates(row: int, col: int) -> bool:
@@ -184,23 +199,4 @@ class TicTacToe:
 
 if __name__ == '__main__':
     game = TicTacToe()
-    game.init()
-    step_game = 0
-    while game:
-        game.show()
-
-        if step_game % 2 == 0:
-            game.human_go()
-        else:
-            game.computer_go()
-
-        step_game += 1
-
-    game.show()
-
-    if game.is_human_win:
-        print("Поздравляем! Вы победили!")
-    elif game.is_computer_win:
-        print("Все получится, со временем")
-    else:
-        print("Ничья.")
+    game.play()
